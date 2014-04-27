f26ab9991b809a75560b012cc38b375153d8fd6b
Wei Shung Chung <wei@unknown60334b1cefcd.gateway.2wire.net>
2014-04-27 16:34:30 -0700

    Rename folders and create pg-common package

---
+++ pg-hbase/src/main/java/wsc/bigdata/playground/App.java
@@ -0,0 +1,13 @@
+package wsc.bigdata.playground;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
